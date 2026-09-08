512cf4e8c1502a44940d42985d922cebbe1a0b99
GeneralTitus03 <abhishek3rawat@gmail.com>
2026-09-08 21:57:42 +0530

    test: add invalid login scenario

--- pages/LoginSignupPage.ts
+++ pages/LoginSignupPage.ts
@@ -6,11 +6,13 @@ export class LoginSignupPage{
     readonly emailInput: Locator;
     readonly passwordInput: Locator;
     readonly loginButton: Locator;
+    readonly loginErrorMessage: Locator;
     constructor(page: Page) {
         this.page= page;
         this.emailInput= page.locator('[data-qa="login-email"]');
         this.passwordInput= page.locator('[data-qa="login-password"]');
         this.loginButton= page.locator('[data-qa="login-button"]');
+        this.loginErrorMessage= page.getByText('Your email or password is incorrect!');
     }
 
     async goto():Promise<void>{
@@ -20,6 +22,6 @@ export class LoginSignupPage{
     async login(email:string, password: string): Promise<void>{
         await this.emailInput.fill(email);
         await this.passwordInput.fill(password);
-        await this.loginButton.click()
+        await this.loginButton.click();
     }
 }
--- tests/loginSignupTest.spec.ts
+++ tests/loginSignupTest.spec.ts
@@ -9,4 +9,11 @@ test.describe('Login tests', ()=>{
         await loginSignupPage.login(users.valid.email, users.valid.password);
         await expect(page.getByText(`Logged in as ${users.valid.expectedUserName}`)).toBeVisible();
     });
+
+    test('User should not be able to login with invalid credentials', async({page})=>{
+        const loginSignupPage= new LoginSignupPage(page);
+        await loginSignupPage.goto();
+        await loginSignupPage.login(users.invalid.email, users.invalid.password);
+        await expect(loginSignupPage.loginErrorMessage).toHaveText(users.invalid.expectedErrorMessage);
+    })
 });
