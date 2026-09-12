85d70d2f4cb089337cfcb9a8a42a6695be1ee10c
GeneralTitus03 <abhishek3rawat@gmail.com>
2026-09-12 16:51:08 +0530

    Refactor product setup with beforeEach

--- tests/productPage.spec.ts
+++ tests/productPage.spec.ts
@@ -2,14 +2,16 @@ import { test,expect } from '../fixtures/base.fixture';
 import { products } from '../data/products';
 
 test.describe('Product page tests', ()=>{
-    test('product search', async({productPage,header,authenticatedPage})=>{
+    test.beforeEach('Navigate to Products', async({header,authenticatedPage})=>{
         await header.goToProducts();
+    });
+
+    test('product search', async({productPage})=>{
         await productPage.searchProduct(products.blueTop.name);
         await expect(productPage.getProduct(products.blueTop.name)).toContainText(products.blueTop.name);
     });
 
-    test('User should be able to add product to the cart', async({authenticatedPage,productPage,header,cartModal})=>{
-        await header.goToProducts();
+    test('User should be able to add product to the cart', async({productPage,cartModal})=>{
         await productPage.searchProduct(products.blueTop.name);
         await productPage.addProductToCart(products.blueTop.name);
         await expect(cartModal.productAddedConfirmationMessage).toBeVisible();
