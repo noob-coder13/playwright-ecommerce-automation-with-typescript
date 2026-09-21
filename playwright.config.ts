a80dfd519b73a6416249251f3f030827c0008dc1
GeneralTitus03 <abhishek3rawat@gmail.com>
2026-09-21 17:28:04 +0530

    ci: separate nightly sharded regression workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Use controlled parallelism on CI */
-  workers: process.env.CI ? 4 : undefined,
+  workers: process.env.NIGHTLY ? 4: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
